8ee30b984a19d72271bb4facf1c1dffed68499b4
Jan Olencki <jan@olencki.pl>
2019-11-05 21:24:26 +0100

    Correct paths and remove non-source files

--- test_post_synthesis/INCA_libs/irun.nc/.timestamp.ts
+++
@@ -1,3 +0,0 @@
-1571846238 /home2/s_jolencki/projASIC/test_post_synthesis/src/cells_typical_corrected.v
-1571846238 /home2/s_jolencki/projASIC/test_post_synthesis/src/keccak_umc.v
-1571843608 /home2/s_jolencki/projASIC/test_pre_synthesis/src/keccak_test.v
--- test_pre_synthesis/INCA_libs/irun.nc/.timestamp.ts
+++
@@ -1,7 +0,0 @@
-1571833515 /home2/s_jolencki/projASIC/test_pre_synthesis/src/round2in1.v
-1571833515 /home2/s_jolencki/projASIC/test_pre_synthesis/src/padder1.v
-1571833515 /home2/s_jolencki/projASIC/test_pre_synthesis/src/keccak.v
-1571833515 /home2/s_jolencki/projASIC/test_pre_synthesis/src/rconst2in1.v
-1571833515 /home2/s_jolencki/projASIC/test_pre_synthesis/src/f_permutation.v
-1571833515 /home2/s_jolencki/projASIC/test_pre_synthesis/src/padder.v
-1571843608 /home2/s_jolencki/projASIC/test_pre_synthesis/src/keccak_test.v
